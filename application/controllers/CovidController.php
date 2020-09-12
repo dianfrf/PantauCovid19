989c25e33cc641f91f60b7d9ab5a604262aa91a0
dianfrf <arifdian56@gmail.com>
2020-09-12 15:22:53 +0700

    Perbaikan kode agar lebih rapi

--- application/controllers/CovidController.php
+++ application/controllers/CovidController.php
@@ -7,12 +7,12 @@
         {
             parent::__construct();
             $this->load->library('getdataapi');
-            $this->load->model('M_pantaucovid');
-            
+            $this->load->model('M_pantaucovid');       
         }     
 
         public function index()
         {
+            $data = [];
             $data['konten'] = "v_datacovid";
             $data['active'] = "Home";
             $data['datacovid'] = $this->getdataapi->getDataCovid();
@@ -23,6 +23,7 @@
 
         public function deteksidini()
         {
+            $data = [];
             $data['konten'] = "v_deteksidini";
             $data['active'] = "Deteksi";
             $this->load->view('layout', $data);
@@ -39,22 +40,38 @@
                 $jawaban6 = $this->input->post('jawaban6');
                 $jawaban7 = $this->input->post('jawaban7');
                 $jawaban8 = $this->input->post('jawaban8');
-                if($jawaban6 == 45 || $jawaban7 == 45 || $jawaban8 == 45) {
-                    redirect('laporsigap');
-                }
-                else if($jawaban1+$jawaban2+$jawaban3+$jawaban4+$jawaban5 >= 45) {
-                    redirect('laporsigap');
-                }
-                else {
+                if (
+                    $this->indikasinonmediscovid($jawaban6, $jawaban7, $jawaban8) 
+                    || $this->indikasimediscovid($jawaban1, $jawaban2, $jawaban3, $jawaban4, $jawaban5)) 
+                {
+                redirect('laporsigap');
+                } else {
                     $this->session->set_flashdata('pesan', '<div class="alert alert-success">Selamat Anda masih sehat. Tetap jaga kesehatan dengan makan teratur dan istirahat cukup.
-                    Jaga jarak dengan sesama dan #TetapDirumahSaja</div>');
+                        Jaga jarak dengan sesama dan #TetapDirumahSaja</div>');
                     redirect('deteksidini');
                 }
             }
         }
 
+        protected function indikasinonmediscovid($jawaban6, $jawaban7, $jawaban8)
+        {
+            if ($jawaban6 == 45 || $jawaban7 == 45 || $jawaban8 == 45) {
+                return true;
+            }
+            return false;
+        }
+
+        protected function indikasimediscovid($jawaban1, $jawaban2, $jawaban3, $jawaban4, $jawaban5)
+        {
+            if ($jawaban1+$jawaban2+$jawaban3+$jawaban4+$jawaban5 >= 45) {
+                return true;
+            }
+            return false;
+        }
+
         public function rumahsakit()
         {
+            $data = [];
             $data['konten'] = "v_rumahsakit";
             $data['active'] = "RS";
             $data['datars'] = $this->getdataapi->getDataHospital();
@@ -63,6 +80,7 @@
 
         public function laporsigap()
         {
+            $data = [];
             $data['konten'] = "v_laporsigap";
             $data['active'] = "Lapor";
             $this->load->view('layout', $data); 
@@ -71,18 +89,12 @@
         public function lapor()
         {
             if ($this->input->post('laporkan')) {
-                if ($this->M_pantaucovid->add_laporan() == TRUE) {
-                    $this->session->set_flashdata('pesan', '<div class="alert alert-success">Sukses melakukan laporan</div>');
-                }
-                else {
-                    $this->session->set_flashdata('pesan', '<div class="alert alert-danger">Gagal melakukan laporan</div>');
-                }
-            }
-            else {
+                $this->M_pantaucovid->add_laporan($this->input->post('nama'), $this->input->post('alamat'), $this->input->post('telepon'));
+                $this->session->set_flashdata('pesan', '<div class="alert alert-success">Sukses melakukan laporan</div>');
+            } else {
                 $this->session->set_flashdata('pesan', '<div class="alert alert-danger">Terjadi kesalahan pada jaringan</div>');
             }
             redirect('laporsigap');
         }
-    
     }
 ?>
--- application/models/M_pantaucovid.php
+++ application/models/M_pantaucovid.php
@@ -3,22 +3,13 @@
     defined('BASEPATH') OR exit('No direct script access allowed');
     
     class M_pantaucovid extends CI_Model {
-        public function add_laporan()
+        public function add_laporan($nama, $alamat, $telepon)
         {
-            $nama       = $this->input->post('nama');
-            $alamat     = $this->input->post('alamat');
-            $telepon    = $this->input->post('telepon');
-            $created    = date('Y-m-d h:i:s');
+            $created = date('Y-m-d h:i:s');
 
             $this->db->query("INSERT INTO hasil_laporan VALUES('', '$nama', '$alamat', '$telepon', '$created')");
-            if ($this->db->affected_rows()>0) {
-                return TRUE;
-            }
-            else {
-                return FALSE;
-            }
-        }
-        
+            return true;
+        }   
     
     }
 ?>
